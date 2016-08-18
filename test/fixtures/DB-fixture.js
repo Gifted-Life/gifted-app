7b35062264a0cd78e3db9b7c5dfe6e095f355cee
Michael Laythe <mrlaythe24@aol.com>
2016-08-18 16:42:48 -0700

    trying to fix travis build issues so that it can connect to db with encryped variable found in .yml file

--- test/fixtures/DB-fixture.js
+++ test/fixtures/DB-fixture.js
@@ -1,14 +1,26 @@
 'use strict';
-const config = require('../../config.js');
+require('dotenv').config();
+let knex;
 
-const knex = require('knex')({
-  client: 'pg',
-  connection: config.TEST_DB,
-  pool: {
-    min: 1,
-    max: 7
-  }
-});
+if (process.env.TEST_DB.length) {
+  knex = require('knex')({
+    client: 'pg',
+    connection: process.env.TEST_DB,
+    pool: {
+      min: 1,
+      max: 7
+    }
+  });
+} else {
+  knex = require('knex')({
+    client: 'pg',
+    connection: TEST_DB,
+    pool: {
+      min: 1,
+      max: 7
+    }
+  });
+}
 
 const bookshelf = require('bookshelf')(knex);
 
