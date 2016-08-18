c9dfba43263f5aa499620e0129685e46fac91089
Michael Laythe <mrlaythe24@aol.com>
2016-08-18 14:32:47 -0700

    creates events & sends back eventID generated when it is made (note: creating table here to handle async nature)

---
+++ server/Events/eventController.js
@@ -0,0 +1,42 @@
+'use strict';
+const bookshelf = require('../../test/fixtures/DB-fixture').bookshelf;
+const knex = require('../../test/fixtures/DB-fixture').knex;
+const Event = require('./eventModel');
+
+const eventController = {};
+
+eventController.createTable = () => {
+  return knex.schema.createTableIfNotExists('events', event => {
+    event.increments('eventID');
+    event.string('title');
+    event.string('location');
+    event.string('time');
+    event.string('comments');
+    event.integer('priceMin');
+    event.integer('priceMax');
+    event.boolean('isMatched');
+    event.string('creator');
+  });
+};
+
+eventController.createEvent = (req, res, next) => {
+  if (!req.body.title || !req.body.location || !req.body.time ||
+      !req.body.comments || !req.body.priceMin || !req.body.priceMax || !req.body.creator) {
+    res.status(401).send('Missing one or more event fields');
+  }
+
+  eventController.createTable()
+    .then( () => {
+      Event.forge(req.body).save().then( result => {
+        console.log('result', result);
+        res.status(201).send({
+          eventID: result.attributes.eventID
+        });
+      });
+    })
+    .catch( err => {
+      res.status(400).send('Error adding event to database:');
+    });
+};
+
+module.exports = eventController;
