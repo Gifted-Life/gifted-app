07d8f3ff8aed3760eac2e95845f8b1284e196447
Michael Laythe <mrlaythe24@aol.com>
2016-08-20 15:35:59 -0700

    grabs all eventids associated with user, still need to grab actual event info for each eventid

--- server/UserEvents/userEventsController.js
+++ server/UserEvents/userEventsController.js
@@ -2,19 +2,63 @@
 const bookshelf = require('../../test/fixtures/DB-fixture').bookshelf;
 const knex = require('../../test/fixtures/DB-fixture').knex;
 const UserEvents = require('./userEventsModel');
+const User = require('../Users/userModel.js');
+const tokenController = require('../Utils/tokenController');
+const _ = require('lodash');
 
 const userEventsController = {};
 
-knex.schema.createTableIfNotExists('user-events', userEvent => {
-  userEvent.increments();
-  userEvent.string('userID');
-  userEvent.integer('eventID');
-  userEvent.string('rsvpStatus');
-})
-.then( () => {
-  UserEvents.forge({ userID: 'me123', eventID: 1234, rsvpStatus: 'pending' }).save().then( result => {
-    console.log('user event successfully added', result);
+userEventsController.createTable = () => {
+  return knex.schema.createTableIfNotExists('user-events', userEvent => {
+    userEvent.increments();
+    userEvent.string('email');
+    userEvent.integer('eventid');
+    userEvent.string('rsvpStatus');
   });
-});
+};  
+
+userEventsController.createUserEventConnection = (req, res, next) => {
+  userEventsController.createTable()
+    .then( () => {
+      UserEvents.forge({ email: req.body.creator, eventid: req.body.eventID, rsvpStatus: 'attending' }).save().then( result => {
+        res.status(201).send({
+          eventID: req.body.eventID
+        });
+      });
+    })
+    .catch( err => {
+      res.status(400).send('Error adding user-event connection.');
+    });
+};
+
+userEventsController.getEvents = (req, res, next) => {
+  UserEvents
+    .query({where: {email: req.body.email}})
+    .fetchAll()
+    .then( events => {
+      if (!events) {
+        return res.status(201).send('No events associated with this user.');
+      }
+      
+      const allEvents = [];
+
+      _.forEach(events.models, (event, key) => {
+        allEvents.push(event.attributes.eventid);
+      });
+
+      //TODO fetch event info for each eventid in allEvents array
+
+      console.log(allEvents);
+      
+      res.status(201).send({
+        id_token: tokenController.createToken(req.body, req.body.emailid),
+        events: allEvents
+      });
+    })
+    .catch( err => {
+      console.log('wdwdwdwd inside err');
+      res.status(400).send('Error fetching events associated with user.');
+    });
+};
 
 module.exports = userEventsController;
