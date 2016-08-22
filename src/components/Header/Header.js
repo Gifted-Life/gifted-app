ee0fdc8f2d4c8bb4cf5d4453cff0122fb7fbf44a
Michael Blanchard <michael.james.blanchard@gmail.com>
2016-08-22 10:54:54 -0700

    added random color to header div, to test out the random generator

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,17 @@
 import React, { PropTypes } from 'react';
 import Logo from './../Logo/Logo';
 
-const Header = props => {
-  return (
-    <div>
-      <Logo imgSrc={props.imgSrc} />
-    </div>
-  );
+const randomColor = `#${Math.ranom().toString(16).substr(-6)}`;
+const headerStyle = {
+  backgroundColor: randomColor,
 };
+
+const Header = props =>
+(
+  <div style={headerStyle}>
+    <Logo imgSrc={props.imgSrc} />
+  </div>
+);
 
 Header.propTypes = {
   imgSrc: PropTypes.string,
