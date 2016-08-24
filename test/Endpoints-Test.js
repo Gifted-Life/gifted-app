70200a0d7453f407da0e61c4807ec678a82ff089
Michael Laythe <mrlaythe24@aol.com>
2016-08-23 21:57:24 -0700

    added tests to simulate situation in where user is invited to event and he/she decides to accept

--- test/Endpoints-Test.js
+++ test/Endpoints-Test.js
@@ -33,7 +33,7 @@ test('Successfully signups user', (t) => {
       }
 
       t.ok(res.body.id_token, 'jwt should exist');
-      t.ok(token.emailid, 'emailid on token should exist');
+      t.ok(token.email, 'emailid on token should exist');
       t.equal(token.admin, false, 'admin should be set to false on jwt upon signup');
       t.same(res.status, 201, 'correct status code was sent');
       destroy(t);
@@ -55,7 +55,7 @@ test('Succesfully creates an event & connects user with event', (t) => {
     .expect(201)
     .then( res => {
       return request(app)
-        .post(`/${name}/event`)
+        .post(`/${email}/event`)
         .send({
           title: 'Christmas Party!',
           location: 'Mom\'s house',
@@ -89,7 +89,7 @@ test('Successfully logins user and returns all events associated with that user'
     .expect(201)
     .then( res => {
       return request(app)
-        .post(`/${name}/event`)
+        .post(`/${email}/event`)
         .send({
           title: 'Christmas Party!',
           location: 'Mom\'s house',
@@ -147,13 +147,69 @@ test('Successfully invites user to event', (t) => {
 });
 
 test('Successfully submits rsvp response to event', (t) => {
+  const user1 = {
+    name: faker.name.firstName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  };
+
+  const user2 = {
+    name: faker.name.firstName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  };
+
+  let token,
+      eventid;
+
   request(app)
-    .put('/me123/24/response')
-    .send({
-      response: 'attending'
+    .post('/user/signup')
+    .send(user1)
+    .expect(201)
+    .then( res => {
+      return request(app)
+        .post('/user/signup')
+        .send(user2)
+        .expect(201)
+        .then( () => {
+          token = res.body.id_token;
+        });
     })
-    .expect(200)
-    .end( (err, res) => {
+    .then( res => {
+      return request(app)
+        .post(`/${user1.email}/event`)
+        .send({
+          title: 'Christmas Party!',
+          location: 'Mom\'s house',
+          time: '8:00pst',
+          comments: 'bring love to everything you do',
+          priceMin: 10,
+          priceMax: 25,
+          isMatched: false,
+          creator: user1.email
+        })
+        .expect(201)
+        .then( res => {
+          eventid = res.body.eventID;
+        });
+    })
+    .then( res => {
+      return request(app)
+        .post(`/event/${eventid}/invite-user`)
+        .send({
+          inviteUser: user2.email
+        })
+        .expect(200);
+    })
+    .then( res => {
+      return request(app)
+        .put(`/${user2.email}/${eventid}/response`)
+        .send({
+          response: 'attending'
+        })
+        .expect(200)
+    })
+    .then( res => {
       t.same(res.status, 200, 'correct status code was sent');
       destroy(t);
     });
