a6d40481f6c4f36a3c9a7862222d3336f89e781e
Brandon Mizuno <brandon.mizuno@gmail.com>
2016-08-30 13:02:51 -0700

    delete existing matches for same event

--- server/EventPartnerMatches/eventPartnerMatchesController.js
+++ server/EventPartnerMatches/eventPartnerMatchesController.js
@@ -17,43 +17,46 @@ eventPartnerMatches.createTable = () => {
 };
 
 eventPartnerMatches.createMatches = (req, res) => {
-  console.log('req.params',req.params);
   UserEvents
-  .query({where: {
-    eventid: req.params.eventID,
-    rsvpStatus: 'attending',
-  }})
-  .fetchAll()
-  .then((collection) => {
-    return collection.map(function(item) {
-      return item.attributes.email;
-    });
-  })
-  .then((attending) => {
-    eventPartnerMatches.createEventPartnerMatches(req, res, shuffle(attending));
-  })
+    .query({where: {
+      eventID: req.params.eventID,
+      rsvpStatus: 'attending',
+    }})
+    .fetchAll()
+    .then((collection) => {
+      return collection.map(function(item) {
+        return item.attributes.email;
+      });
+    })
+    .then((attending) => {
+      eventPartnerMatches.createEventPartnerMatches(req, res, shuffle(attending));
+    })
 }
 
 eventPartnerMatches.createEventPartnerMatches = (req, res, orderedMatches) => {
   eventPartnerMatches.createTable()
     .then( () => {
-      const toSend = [];
-      var results = orderedMatches.map( (partner1, i) => {
+      EventPartnerMatches
+        .query({where: {eventID: req.params.eventID}})
+        .destroy();
+
+      const matches = [];
+      var promisedMatches = orderedMatches.map( (partner1, i) => {
         return new Promise( resolve => {
           EventPartnerMatches.forge({
             eventID: req.params.eventID,
             partner1: partner1,
             partner2: orderedMatches[i + 1] || orderedMatches[0],
           }).save().then( result => {
-            resolve(toSend.push(result));
+            resolve(matches.push(result));
           })
         });
       })
 
-      Promise.all(results).then(() => {
+      Promise.all(promisedMatches).then(() => {
         res.status(200);
         res.writeHead(200, {'Content-Type': 'application/json' });
-        res.write(JSON.stringify({matches: toSend}));
+        res.write(JSON.stringify({matches: matches}));
         res.end();
       });
     })
