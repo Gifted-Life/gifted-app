082b4fa2f7de5a359a95d0d47a759a303331902d
jerrymao15 <jerryy.mao@gmail.com>
2016-08-31 13:26:46 -0700

    why did i change this

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store.js';
-// import { createStore } from 'redux';
-// import reducers from './reducers';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
 import App from './components/App';
 
-// const store = createStore(reducers);
+const store = createStore(reducers);
 
 render(
   <Provider store={store}>
