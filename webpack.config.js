90fa53c4dcca9ab967a780b29668dd6e1a00b575
jerrymao15 <jerryy.mao@gmail.com>
2016-08-31 17:00:06 -0700

    got rid of resolve rl

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,6 @@ module.exports = {
       loaders: [
         'style?sourceMap',
         'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
-        'resolve-url',
         'sass?sourceMap',
       ],
     },
