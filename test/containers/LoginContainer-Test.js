f7dc81e58e0079835bc4986f6668e8c761e36e77
Sandra Winkler <sandra.winkler.h@gmail.com>
2016-09-09 23:58:02 -0700

    updated login container test file to include testing outcomes of user input

--- test/containers/LoginContainer-Test.js
+++ test/containers/LoginContainer-Test.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { mount } from 'enzyme';
 import { expect } from 'chai';
+import nock from 'nock';
 import Login from './../../src/containers/LoginContainer/LoginContainer.js';
 import FormInput from './../../src/components/FormInputs/FormInput.js';
 import RectangleButton from './../../src/components/RectangleButton/RectangleButton.js';
 import ErrorMessage from './../../src/components/ErrorMessage/ErrorMessage.js';
+import reducers from './../../src/reducers/index.js';
+
+const fakeStore = function (injectedState) {
+  return createStore(
+    reducers,
+    injectedState,
+    applyMiddleware(thunkMiddleware)
+  );
+};
+
+const testURL = 'http://FAKEURL';
 
 describe('Login Component', () => {
-  const wrapper = mount(<Login />);
+  const testStore = fakeStore({});
+  const wrapper = mount(
+    <Provider store={testStore}>
+      <Login />
+    </Provider>
+  );
 
   it('should render two form input components', () => {
     expect(wrapper.find(FormInput)).to.have.length(2);
@@ -37,23 +57,97 @@ describe('Login Component', () => {
     expect(wrapper.find(ErrorMessage)).to.have.length(0);
   });
 
+  it('should set the state with email input on each change', () => {
+    const testStore = fakeStore({});
+    const wrapper = mount(
+      <Provider store={testStore}>
+        <Login />
+      </Provider>
+    );
+
+    wrapper.find('input [type="email"]').simulate('change', { target: { value: 'a' } });
+    expect(testStore.getState().userState.email).to.equal('a');
+  });
+
+  it('should set state with password input on each change', () => {
+    const testStore = fakeStore({});
+    const wrapper = mount(
+      <Provider store={testStore}>
+        <Login />
+      </Provider>
+    );
+    wrapper.find('input [type="password"]').simulate('change', { target: { value: 'b' } });
+    expect(testStore.getState().userState.password).to.equal('b');
+  });
+
   xit('should display an error message if incorrect email or password', () => {
-
+    // Trouble testing because this click dispatches an async action that returns
+    // after the test has checked the component
+    nock(testURL)
+      .log(console.log)
+      .post('/user/login')
+      .reply(400, 'Incorrect email or password');
+    
+    const testStore = fakeStore(
+      { userState: {
+        email: 'nope@hi.com',
+        password: 'wrong',
+        emptyLoginField: false },
+      });
+    const wrapper = mount(
+      <Provider store={testStore}>
+        <Login />
+      </Provider>
+    );
+    wrapper.find('button').simulate('click');
+    expect(wrapper.find('#loginError')).to.have.length(1);
   });
 
-  xit('should dispatch a password input action on each user input', () => {
-
+  it('should display an error message if empty email field was submitted', () => {
+    const testStore = fakeStore(
+      { userState: {
+        email: '',
+        password: 'hola',
+        emptyLoginField: false },
+      });
+    const wrapper = mount(
+      <Provider store={testStore}>
+        <Login />
+      </Provider>
+    );
+    wrapper.find('button').simulate('click');
+    expect(wrapper.find('#emptyLoginInputError')).to.have.length(1);
   });
 
-  xit('should dispatch an email input action on each user input', () => {
-   
+  it('should display an error message if empty password field was submitted', () => {
+    const testStore = fakeStore(
+      { userState: {
+        email: 'hello@hi.com',
+        password: '',
+        emptyLoginField: false },
+      });
+    const wrapper = mount(
+      <Provider store={testStore}>
+        <Login />
+      </Provider>
+    );
+    wrapper.find('button').simulate('click');
+    expect(wrapper.find('#emptyLoginInputError')).to.have.length(1);
   });
 
-  xit('should set the state with email input on each change', () => {
-
-  });
-
-  xit('should set state with password input on each change', () => {
-
+  it('should display an error message if empty password and email fields are submitted', () => {
+    const testStore = fakeStore(
+      { userState: {
+        email: '',
+        password: '',
+        emptyLoginField: false },
+      });
+    const wrapper = mount(
+      <Provider store={testStore}>
+        <Login />
+      </Provider>
+    );
+    wrapper.find('button').simulate('click');
+    expect(wrapper.find('#emptyLoginInputError')).to.have.length(1);
   });
 });
