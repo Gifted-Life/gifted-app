b7a874bccf6e990ca8bd288b3ea2931410dde131
Sandra Winkler <sandra.winkler.h@gmail.com>
2016-09-13 11:37:47 -0700

    removed tests pertaining to login view

--- test/containers/LoginContainer-Test.js
+++ test/containers/LoginContainer-Test.js
@@ -5,10 +5,7 @@ import thunkMiddleware from 'redux-thunk';
 import { mount } from 'enzyme';
 import { expect } from 'chai';
 import nock from 'nock';
-import Login from './../../src/containers/LoginContainer/LoginContainer.js';
-import FormInput from './../../src/components/FormInputs/FormInput.js';
-import RectangleButton from './../../src/components/RectangleButton/RectangleButton.js';
-import ErrorMessage from './../../src/components/ErrorMessage/ErrorMessage.js';
+import LoginContainer from './../../src/containers/LoginContainer/LoginContainer.js';
 import reducers from './../../src/reducers/index.js';
 
 const fakeStore = function (injectedState) {
@@ -21,47 +18,13 @@ const fakeStore = function (injectedState) {
 
 const testURL = 'http://FAKEURL';
 
-describe('Login Component', () => {
-  const testStore = fakeStore({});
-  const wrapper = mount(
-    <Provider store={testStore}>
-      <Login />
-    </Provider>
-  );
+describe('Login Container Component', () => {
 
-  it('should render two form input components', () => {
-    expect(wrapper.find(FormInput)).to.have.length(2);
-  });
-
-  it('should render one email input', () => {
-    expect(wrapper.find('input [type="email"]')).to.have.length(1);
-  });
-
-  it('should render one password input', () => {
-    expect(wrapper.find('input [type="password"]')).to.have.length(1);
-  });
-
-  it('should render a login header', () => {
-    expect(wrapper.find('h1')).to.have.length(1);
-  });
-
-  it('should render a submit button', () => {
-    expect(wrapper.find(RectangleButton)).to.have.length(1);
-  });
-
-  it('should render a form', () => {
-    expect(wrapper.find('form')).to.have.length(1);
-  });
-
-  it('should not initially render an error message', () => {
-    expect(wrapper.find(ErrorMessage)).to.have.length(0);
-  });
-
-  it('should set the state with email input on each change', () => {
+  it('should set state with email input on each change', () => {
     const testStore = fakeStore({});
     const wrapper = mount(
       <Provider store={testStore}>
-        <Login />
+        <LoginContainer />
       </Provider>
     );
 
@@ -73,9 +36,10 @@ describe('Login Component', () => {
     const testStore = fakeStore({});
     const wrapper = mount(
       <Provider store={testStore}>
-        <Login />
+        <LoginContainer />
       </Provider>
     );
+
     wrapper.find('input [type="password"]').simulate('change', { target: { value: 'b' } });
     expect(testStore.getState().userState.password).to.equal('b');
   });
@@ -96,11 +60,15 @@ describe('Login Component', () => {
       });
     const wrapper = mount(
       <Provider store={testStore}>
-        <Login />
+        <LoginContainer />
       </Provider>
     );
     wrapper.find('button').simulate('click');
     expect(wrapper.find('#loginError')).to.have.length(1);
+  });
+
+  xit('should redirect to user page if correct email and password', () => {
+
   });
 
   it('should display an error message if empty email field was submitted', () => {
@@ -112,7 +80,7 @@ describe('Login Component', () => {
       });
     const wrapper = mount(
       <Provider store={testStore}>
-        <Login />
+        <LoginContainer />
       </Provider>
     );
     wrapper.find('button').simulate('click');
@@ -128,7 +96,7 @@ describe('Login Component', () => {
       });
     const wrapper = mount(
       <Provider store={testStore}>
-        <Login />
+        <LoginContainer />
       </Provider>
     );
     wrapper.find('button').simulate('click');
@@ -144,7 +112,7 @@ describe('Login Component', () => {
       });
     const wrapper = mount(
       <Provider store={testStore}>
-        <Login />
+        <LoginContainer />
       </Provider>
     );
     wrapper.find('button').simulate('click');
