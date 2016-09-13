4f6b96ae61f83c30a81cead821997ce4b6ef52f5
jerrymao15 <jerryy.mao@gmail.com>
2016-09-13 10:25:00 -0700

    tabpane component

---
+++ test/components/TabPane-Test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
+import TabPane from './../../src/components/TabPane/TabPane';
+
+describe('TabPane component', function() {
+
+
+  
+})
